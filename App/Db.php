a9f3680ac9134ddc4cc545748acf001e1289f23f
Yzat Turganbaev <yzatsn@gmail.com>
2016-09-18 00:20:05 +0600

    метод execute() класса Db

--- App/Db.php
+++ App/Db.php
@@ -7,17 +7,9 @@
 		public function __construct(){
 			$this->dbh=new \PDO('mysql:host=127.0.0.1;dbname=oop_yt','root','');
 		}
-//		public function execute($sql){
-//			$sth=$this->dbh->prepare($sql);
-//			$res=$sth->execute();
-//			return $res;
-//		}
-//		public function query($sql){
-//			$sth=$this->dbh->prepare($sql);
-//			$res=$sth->execute();
-//			if(false!==$res){
-//				return $sth->fetchAll();
-//			}
-//			return [];
-//		}
+		public function execute($sql){
+			$sth=$this->dbh->prepare($sql);
+			$res=$sth->execute();
+			return $res;
+		}
 	}
