f75596fcbacfb97d29eb712bbe4d0c0dd8f2c09f
Yzat Turganbaev <yzatsn@gmail.com>
2016-09-27 15:26:53 +0600

    статическое свойство

--- App/Models/User.php
+++ App/Models/User.php
@@ -1,8 +1,17 @@
 <?php
 	namespace App\Models;
 
+	use App\Db;
 
 	class User
 	{
-
+		public static $table='users';
+		
+		public $email;
+		public $name;
+		public static function findAll(){
+			$db=new \App\Db();
+			$users=$db->query('SELECT * FROM '.self::$table ,'App\Models\User');
+			return $users;
+		}
 	}
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 require __DIR__.'/autoload.php';
 
-$db=new \App\Db();
-//	$res=$db->execute('CREATE TABLE foo (id SERIAL)');
-	$data=$db->query('SELECT * FROM albums');
-//	echo "<pre>";
-//		print_r($data);
-//	echo "</pre>";
+$data= \App\Models\User::findAll();
+var_dump($data);
